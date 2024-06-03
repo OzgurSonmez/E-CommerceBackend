9a946dc2966909f8143ea850f214993921c17cad
OzgurSonmez <ozgursonmez00@gmail.com>
2024-06-03 10:37:02 +0300

    Init Commit

---
+++ DataAccess/OracleDbContext.cs
@@ -0,0 +1,48 @@
+﻿using System.Data;
+using Oracle.ManagedDataAccess.Client;
+
+namespace DataAccess
+{
+    public class OracleDbContext
+    {
+        private readonly string _connectionString;
+
+        public OracleDbContext(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public T ExecuteFunction<T>(string functionName, OracleParameter[] parameters)
+        {
+            using (var connection = new OracleConnection(_connectionString))
+            {
+                using (var command = new OracleCommand(functionName, connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    // Add parameters
+                    foreach (var param in parameters)
+                    {
+                        command.Parameters.Add(param);
+                    }
+
+                    // Add return parameter
+                    var returnParameter = new OracleParameter("return_value", OracleDbType.Int64, ParameterDirection.ReturnValue);
+                    command.Parameters.Add(returnParameter);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+
+                    var result = command.Parameters["return_value"].Value;
+
+                    if (result == DBNull.Value)
+                    {
+                        throw new Exception("No data found.");
+                    }
+
+                    return (T)Convert.ChangeType(result, typeof(T));
+                }
+            }
+        }
+    }
+}
---
+++ WebAPI/Program.cs
@@ -0,0 +1,36 @@
+using DataAccess;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Configure dependency injection
+builder.Services.AddSingleton<OracleDbContext>(sp =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    return new OracleDbContext(connectionString);
+});
+
+//builder.Services.AddScoped<EMailDal>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
